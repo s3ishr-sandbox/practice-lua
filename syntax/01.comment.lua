99782c773c121a11c66049f528f451fa2ab0764f
Shun ISHIHARA <s.ishihara@jaist.ac.jp>
2016-05-12 23:59:28 +0900

    practice: comment syntax

---
+++ syntax/01.comment.lua
@@ -0,0 +1,23 @@
+#!/usr/bin/env lua-5.2
+
+-- Comment
+
+--[[
+  multiline comment
+]]
+
+--[[
+  --[=[
+    nested comment (can insert any character)
+  ]=]
+]]
+
+-- Code comment out technique
+
+--[[
+  print("Hello, World!")
+--]]
+
+----[[
+  print("Hello, World!")
+--]]
